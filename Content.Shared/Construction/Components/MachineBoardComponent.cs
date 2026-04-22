d14d180e3c2ff33b32f73ca6a65b943ae2b73649
EckoAurum <202461799+EckoAurum@users.noreply.github.com>
2026-04-23 08:15:17 +0900

    Large Machine Frame Systems | Large Thruster Construction/Upgrading (#3703)

--- Content.Shared/Construction/Components/MachineBoardComponent.cs
+++ Content.Shared/Construction/Components/MachineBoardComponent.cs
@@ -47,6 +47,10 @@ public sealed partial class MachineBoardComponent : Component
     /// </summary>
     [DataField]
     public bool Flatpackable = true;
+
+    /// Mono - sets the framesize this board can go into
+    [DataField]
+    public string? FrameSize = null;
 }
 
 [DataDefinition, Serializable]
