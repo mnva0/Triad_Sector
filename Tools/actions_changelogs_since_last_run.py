b5941afc1a4a493c89009106e26e0ccca6c79a51
Triad-Sector <harrisonethan98@gmail.com>
2026-04-25 05:48:47 +0700

    Unhardcode CHANGELOG_FILE path
    Changed CHANGELOG_FILE to be set from environment variable.

--- Tools/actions_changelogs_since_last_run.py
+++ Tools/actions_changelogs_since_last_run.py
@@ -21,8 +21,8 @@ GITHUB_API_URL = os.environ.get("GITHUB_API_URL", "https://api.github.com")
 # https://discord.com/developers/docs/resources/webhook
 DISCORD_SPLIT_LIMIT = 2000
 DISCORD_WEBHOOK_URL = os.environ.get("DISCORD_WEBHOOK_URL")
-
-CHANGELOG_FILE = "Resources/Changelog/Triad.yml" # Triad
+# Unhardcode
+CHANGELOG_FILE = os.environ.get("CHANGELOG_DIR")
 
 TYPES_TO_EMOJI = {"Fix": "🐛", "Add": "🆕", "Remove": "❌", "Tweak": "⚒️"}
 
